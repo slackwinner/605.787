29bde50fd6c91a4a88f07ebc5609d3dda8adcc78
Dane <danemagbuhos@gmail.com>
2024-09-04 17:23:33 -0400

    Add Assignment 4 Implementations

--- rmagbuh1_mod4_solution/script.js
+++ rmagbuh1_mod4_solution/script.js
@@ -33,32 +33,89 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+(function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  // See Lecture 50, part 1
+  console.log("Executing Assignment 4: Regular Requirements")
+  for (var currName = 0; currName < names.length; currName++) {
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    var firstLetter = names[currName].charAt(0).toLowerCase();
 
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    if (firstLetter === 'j') {
+        byeSpeaker.speak(names[currName])
+    } else {
+        helloSpeaker.speak(names[currName])
+    }
   }
-}
+
+
+  // Additional Requirements from Assignment 4 document
+  // Create and pass values to the map function
+  function speakerMap(currName) {
+    // Grab first letter of current name
+    var firstLetter = currName.charAt(0).toLowerCase();
+
+    // Invoke corresponding speaker function
+    if (firstLetter === 'j') {
+      return byeSpeaker.speakSimple(currName)
+    } else {
+      return helloSpeaker.speakSimple(currName)
+    }
+  }
+  // Set map function attribute and print results to console
+  console.log("Executing Assignment 4: Additional Requirements (Map Function)")
+  var mapNames = names.map(speakerMap);
+  for (var currName = 0; currName < names.length; currName++) {
+    console.log(mapNames[currName]);
+  }
+
+
+  // Bonus/Optional Requirements from Assignment 4 document
+  // Create and pass values to the reduce function
+  function speakerReduce(accumulator, currName) {
+    // Grab first letter of current name
+    var firstLetter = currName.charAt(0).toLowerCase();
+
+    // Invoke corresponding speaker function
+    if (firstLetter === 'j') {
+      accumulator.bye.push(byeSpeaker.speakSimple(currName));
+    } else {
+      accumulator.hello.push(helloSpeaker.speakSimple(currName));
+    }
+    return accumulator;
+  }
+
+  // Set reduce function attribute and print results to console
+  console.log("Executing Assignment 4: Bonus/Optional Requirements (Reduce Function)")
+  var storageArr = {hello: [], bye: []};
+  var reduceNames = names.reduce(speakerReduce, storageArr);
+
+  for(var currHello in reduceNames.hello) {
+    console.log(reduceNames.hello[currHello]);
+  }
+
+  for(var currBye in reduceNames.bye) {
+    console.log(reduceNames.bye[currBye]);
+  }
+
+})();
+
+
+
