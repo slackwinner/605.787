a2b0a9fff7422050abeed2d7ca7889854bc482e0
Dane <danemagbuhos@gmail.com>
2024-09-18 19:15:07 -0400

    Fix Spacing Issue in JS File for Assignment 7

--- rmagbuh1_mod7_solution/app.js
+++ rmagbuh1_mod7_solution/app.js
@@ -1,30 +1,29 @@
 (function () {
-    'use strict';
+'use strict';
 
-    angular.module('ShoppingListCheckOff', [])
-    .controller('ToBuyController', ToBuyController)
-    .controller('AlreadyBoughtController', AlreadyBoughtController)
-    .service('ShoppingListService', ShoppingListService);
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListService', ShoppingListService);
 
-    ToBuyController.$inject = ['ShoppingListService'];
-    function ToBuyController(ShoppingListService) {
-        // TODO: Add Implementations Here
-        // Note: Serves as a middleman between Frontend and JS for buy interactions
-        var buyList = this;
-    }
-
-    AlreadyBoughtController.$inject = [ShoppingListService];
-    function AlreadyBoughtController(ShoppingListService) {
-        // TODO: Add Implementations Here
-        // Note: Serves as a middleman between Frontend and JS for bought interactions
-        var boughtList = this;
-    }
-
-    function ShoppingListService() {
-        // TODO: Add Implementations Here
-        // Note: Provides business function logic for shopping services
-        var service = this;
-
+ToBuyController.$inject = ['ShoppingListService'];
+function ToBuyController(ShoppingListService) {
+    // TODO: Add Implementations Here
+    // Note: Serves as a middleman between Frontend and JS for buy interactions
+    var buyList = this;
 }
 
+AlreadyBoughtController.$inject = [ShoppingListService];
+function AlreadyBoughtController(ShoppingListService) {
+    // TODO: Add Implementations Here
+    // Note: Serves as a middleman between Frontend and JS for bought interactions
+    var boughtList = this;
+}
+
+function ShoppingListService() {
+    // TODO: Add Implementations Here
+    // Note: Provides business function logic for shopping services
+    var service = this;
+
+}
 })();
