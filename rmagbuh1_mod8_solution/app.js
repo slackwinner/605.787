2392b1fc7c94a7fe7bb785c24fdc05426f1dc1cc
Dane <danemagbuhos@gmail.com>
2024-09-25 18:32:34 -0400

    Setup Assignment 8 Files

---
+++ rmagbuh1_mod8_solution/app.js
@@ -0,0 +1,10 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController);
+
+// TODO: Add Controller Implementations here
+
+}
+})();
