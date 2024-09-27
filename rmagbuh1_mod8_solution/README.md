3436a52b95d8639aa3e04c97489e28d5545ef3b6
Dane <danemagbuhos@gmail.com>
2024-09-27 17:08:53 -0400

    Add Javascript Implementations for Assignment 8

---
+++ rmagbuh1_mod8_solution/README.md
@@ -0,0 +1,17 @@
+# Assignment 8 Notes
+
+Below are the sources that were used to complete this assignment:
+- Starter Codebase
+    - [Assignment 3 (Starter)](https://github.com/jhu-ep-coursera/fullstack-course5/tree/master/assignments/assignment3/assignment3-starter-code)
+
+- Lectures
+    - [Lecture 24](https://github.com/jhu-ep-coursera/fullstack-course5/tree/master/examples/Lecture24)
+    - [Lecture 25](https://github.com/jhu-ep-coursera/fullstack-course5/tree/master/examples/Lecture25)
+    - [Lecture 26](https://github.com/jhu-ep-coursera/fullstack-course5/tree/master/examples/Lecture26)
+    - [Lecture 30](https://github.com/jhu-ep-coursera/fullstack-course5/tree/master/examples/Lecture30)
+
+- Online
+    - [JavaScript Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+    - [Angular JS Documentation](https://docs.angularjs.org/guide)
+    - [Stack Overflow: Iterating Over Object Literals (Stack Overflow)](https://stackoverflow.com/questions/9354834/iterate-over-object-literal-values)
+    - [w3: Java String Includes](https://www.w3schools.com/jsref/jsref_includes.asp)
--- rmagbuh1_mod8_solution/app.js
+++ rmagbuh1_mod8_solution/app.js
@@ -2,9 +2,69 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController', NarrowItDownController);
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com");
 
-// TODO: Add Controller Implementations here
+function FoundItemsDirective() {
+    var ddo = {
+        templateUrl: 'foundItems.html',
+        scope: {
+            searchResults: '<',
+            onRemove: '&'
+        },
+    };
+    return ddo;
+}
 
+NarrowItDownController.$inject = ["MenuSearchService"];
+function NarrowItDownController(MenuSearchService) {
+    var narrowCon = this;
+
+    // Grabs and filters out menu items based on search input
+    narrowCon.searchMenu = function (searchInput) {
+        var results = MenuSearchService.getMatchedMenuItems(searchInput);
+        results.then(function (response){
+            narrowCon.searchResults = response;
+            console.log(narrowCon.searchResults);
+        }).catch(function (error) {
+            console.log("Error Msg: " + error);
+        });
+    }
+
+    // Remove certain items from search results
+    narrowCon.removeItem = function (index) {
+        narrowCon.searchResults.splice(index, 1);
+    }
+};
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+    
+    service.getMatchedMenuItems = function (searchInput) {
+        return $http({
+            method: "GET",
+            url: (ApiBasePath + "/menu_items.json")
+
+        }).then (function (result) {
+            var foundResults = [];
+            var items = result.data;
+            // Grab current category from results
+            for(var currCategory in items) {
+                var category = items[currCategory].menu_items;
+                // Traverse and search through items within current category
+                for (var currItem in category) {
+                    var description = category[currItem].description;
+                    // Does the description have the search term in it? 
+                    if(description.includes(searchInput)) {
+                        foundResults.push(category[currItem]);
+                    }
+                }
+            }
+            return foundResults;
+        });
+    }
 }
 })();
