fd2dba71fb6c618b1d262a2b8e3e718bacc6558a
Dane <danemagbuhos@gmail.com>
2024-10-09 18:06:54 -0400

    Remove Quantity Random Generator for Assignment 7
    Removing the RNG feature in favor of setting default values
    for each item.

--- rmagbuh1_mod7_solution/app.js
+++ rmagbuh1_mod7_solution/app.js
@@ -3,12 +3,13 @@
 
 // Item list setup variables
 var itemTypes = [
-    ["Cookies", 2.00], ["Milk", 3.00], ["Chips", 1.00], ["Egg(s)", 4.00],
-    ["Frozen Pizza(s)", 5.00], ["LEGO Star Wars Battle Pack(s)", 10.00], 
-    ["Kit Kat(s)", 2.00], ["Pen(s)", 1.00]
+    ["Cookies", 2.00, 8], ["Milk", 3.00, 6], ["Chips", 1.00, 2], ["Egg(s)", 4.00, 5],
+    ["Frozen Pizza(s)", 5.00, 4], ["LEGO Star Wars Battle Pack(s)", 10.00, 5], 
+    ["Kit Kat(s)", 2.00, 2], ["Pen(s)", 1.00, 4]
 ];
 var item_name_idx = 0;
 var item_price_idx = 1;
+var item_quantity_idx = 2;
 
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
@@ -71,12 +72,10 @@ function ShoppingListService() {
     service.createList = function (itemTypes) {
         // Auto generate list
         for(let curr_item = 0; curr_item < itemTypes.length; curr_item++) {
-            // Randomly choose quantity for each item
-            var quantity_rand = Math.floor(Math.random() * itemTypes.length) + 1;
             var item = {
                 name: itemTypes[curr_item][item_name_idx],
-                quantity: quantity_rand,
-                pricePerItem: itemTypes[curr_item][item_price_idx]
+                pricePerItem: itemTypes[curr_item][item_price_idx],
+                quantity: itemTypes[curr_item][item_quantity_idx]
             };
             console.log(item);
             buyList.push(item);
