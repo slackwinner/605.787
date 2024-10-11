6e773bacb45cddad2db68f0bae3c9fa8e3d61f8a
Dane <danemagbuhos@gmail.com>
2024-10-11 12:26:14 -0400

    Replace Global Variables for Assignment 7
    This commit contains updates to shift around the global variables
    into their respective controllers

--- rmagbuh1_mod7_solution/app.js
+++ rmagbuh1_mod7_solution/app.js
@@ -1,16 +1,6 @@
 (function () {
 'use strict';
 
-// Item list setup variables
-var itemTypes = [
-    ["Cookies", 2.00, 8], ["Milk", 3.00, 6], ["Chips", 1.00, 2], ["Egg(s)", 4.00, 5],
-    ["Frozen Pizza(s)", 5.00, 4], ["LEGO Star Wars Battle Pack(s)", 10.00, 5], 
-    ["Kit Kat(s)", 2.00, 2], ["Pen(s)", 1.00, 4]
-];
-var item_name_idx = 0;
-var item_price_idx = 1;
-var item_quantity_idx = 2;
-
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
@@ -20,6 +10,13 @@ angular.module('ShoppingListCheckOff', [])
 ToBuyController.$inject = ['ShoppingListService'];
 function ToBuyController(ShoppingListService) {
     var buy = this;
+
+    // Sets up a list full of items
+    var itemTypes = [
+        ["Cookies", 2.00, 8], ["Milk", 3.00, 6], ["Chips", 1.00, 2], ["Egg(s)", 4.00, 5],
+        ["Frozen Pizza(s)", 5.00, 4], ["LEGO Star Wars Battle Pack(s)", 10.00, 5], 
+        ["Kit Kat(s)", 2.00, 2], ["Pen(s)", 1.00, 4]
+    ];
 
     // Create and populate buy list items
     ShoppingListService.createList(itemTypes);
@@ -54,6 +51,11 @@ function ShoppingListService() {
     var buyList = [];
     var boughtList = [];
 
+    // Item index setup variables
+    var item_name_idx = 0;
+    var item_price_idx = 1;
+    var item_quantity_idx = 2;
+
     service.getBuyList = function () {
         // Return current items in buy list
         return buyList;
@@ -70,7 +72,7 @@ function ShoppingListService() {
     };
 
     service.createList = function (itemTypes) {
-        // Auto generate list
+        // Generates list of items to buy
         for(let curr_item = 0; curr_item < itemTypes.length; curr_item++) {
             var item = {
                 name: itemTypes[curr_item][item_name_idx],
